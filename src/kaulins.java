b1d775f4cb4accd7057d86a3cae661cf3e00afe8
Toms Brašs <Brass@lvt.local>
2023-03-08 08:16:48 +0200

    Iespēja kauliņu mest vairākas reizes

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,8 +1,17 @@
+import java.util.Random;
 
 public class kaulins {
-
+	static void mestKaulinu(int reizes){
+		int skaitlis;
+		Random rand = new Random();
+		for(int i=0; i<=reizes; i++){
+			skaitlis = rand.nextInt(6)+1;
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
+		}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		mestKaulinu(2);
 
 	}
 
