e4c761c407258ce4942218537d396148b900e3d8
Toms Brašs <Brass@lvt.local>
2023-03-08 08:27:02 +0200

    Ieviesta iespēja lietotājam pašam ievadīt, cik reizes mest kauliņu

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,18 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class kaulins {
-	static void mestKaulinu(int reizes){
-		int skaitlis;
+	public static void main(String[] args) {
+		int skaitlis, reizes;
 		Random rand = new Random();
-		for(int i=0; i<=reizes; i++){
-			skaitlis = rand.nextInt(6)+1;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes metîsi kauliòu?");
+		reizes = scan.nextInt();
+		for(int i = 1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
 		}
-	public static void main(String[] args) {
-		mestKaulinu(4);
-
-	}
-
 }
