021ba4cd991d4055065ff2a3dcc7e5d9909899b7
Toms Brašs <Brass@lvt.local>
2023-03-20 15:26:49 +0200

    Novērsta iespēja mest negatīvu reižu skaitu

--- src/kaulins.java
+++ src/kaulins.java
@@ -5,17 +5,20 @@ public class kaulins {
 	static void mestKaulinu(int reizes){
 		int skaitlis;
 		Random rand = new Random();
+		
 		for(int i = 1; i<=reizes; i++){
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
-			}
+		}
 	}
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Cik reizes metîsi kauliòu?");
-		reizes = scan.nextInt();
+		do{
+			System.out.println("Cik reizes metîsi kauliòu?");
+			reizes = scan.nextInt();
+		}while(reizes < 1);
 		mestKaulinu(reizes);
-	scan.close();
+		scan.close();
 		}
 }
